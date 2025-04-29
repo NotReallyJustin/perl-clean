fca261f8b6b19898d5fa42c4f0d5358c3d8855f0
nad <nad2040@gmail.com>
2025-04-29 19:45:20 -0400

    simplify match in taint demo

--- taint3.pl
+++ taint3.pl
@@ -11,7 +11,7 @@ chomp($input);
 
 # Taint mode will flag this as unsafe
 # unless we "untaint" it by validating and extracting it
-($input =~ m{^(.*)$}) ? $input = $1 : die "Invalid input: potential security risk.\n";
+if ($input =~ m{^(.*)$}) { $input = $1 }
 print "Listing files in: $input\n";
 system("ls", "-l", $input);
 
